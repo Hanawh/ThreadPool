39a170b14a0043f9fadb5c2642789ce552ebd11c
Jakob Progsch <jakob@progsch.net>
2014-09-26 12:32:41 +0200

    changed typedef to using

--- ThreadPool.h
+++ ThreadPool.h
@@ -22,12 +22,14 @@ private:
     // need to keep track of threads so we can join them
     std::vector< std::thread > workers;
     // the task queue
-    std::queue< std::function<void()> > tasks;
+    std::queue< std::function<void()> > tasks; //接受任意原型是 void() 的函数或是函数对象,不带参数且没有返回值
     
     // synchronization
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop;
+    std::mutex queue_mutex; //实际上是锁 保证任务的添加和移除的互斥性
+    //提供了独占所有权的特性——即不支持递归地对 std::mutex 对象上锁，而 std::recursive_lock 则可以递归地对互斥量对象上锁
+    std::condition_variable condition; 
+    //条件变量提供了两类操作：wait和notify 
+    bool stop; //保证获取task的同步性
 };
  
 // the constructor just launches some amount of workers
@@ -42,13 +44,13 @@ inline ThreadPool::ThreadPool(size_t threads)
                 {
                     std::function<void()> task;
 
-                    {
+                    {   //获取一个待执行的task
                         std::unique_lock<std::mutex> lock(this->queue_mutex);
                         this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
+                            [this]{ return this->stop || !this->tasks.empty(); }); //wait直到有task
                         if(this->stop && this->tasks.empty())
                             return;
-                        task = std::move(this->tasks.front());
+                        task = std::move(this->tasks.front()); //取一个task 将传入的值转为右值
                         this->tasks.pop();
                     }
 
@@ -63,15 +65,15 @@ template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args) 
     -> std::future<typename std::result_of<F(Args...)>::type>
 {
-    typedef typename std::result_of<F(Args...)>::type return_type;
+    using return_type = typename std::result_of<F(Args...)>::type;
 
-    auto task = std::make_shared< std::packaged_task<return_type()> >(
-            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+    auto task = std::make_shared< std::packaged_task<return_type()> >(//packaged_task是任务函数的封装类，通过get_future获取future，然后通过future可以获取函数的返回值(future.get())
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...) //forward不改变最初传入的类型的引用类型
         );
         
     std::future<return_type> res = task->get_future();
     {
-        std::unique_lock<std::mutex> lock(queue_mutex);
+        std::unique_lock<std::mutex> lock(queue_mutex); //对当前块的语句加锁
 
         // don't allow enqueueing after stopping the pool
         if(stop)
@@ -79,7 +81,7 @@ auto ThreadPool::enqueue(F&& f, Args&&... args)
 
         tasks.emplace([task](){ (*task)(); });
     }
-    condition.notify_one();
+    condition.notify_one(); //唤醒一个线程执行
     return res;
 }
 
@@ -90,9 +92,9 @@ inline ThreadPool::~ThreadPool()
         std::unique_lock<std::mutex> lock(queue_mutex);
         stop = true;
     }
-    condition.notify_all();
-    for(std::thread & worker: workers)
-        worker.join();
+    condition.notify_all(); //唤醒所有线程执行
+    for(std::thread &worker: workers) 
+        worker.join(); //等待任务结束
 }
 
 #endif
--- example.cpp
+++ example.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
-
 #include "ThreadPool.h"
-
 int main()
 {
     
-    ThreadPool pool(4);
-    std::vector< std::future<int> > results;
+    ThreadPool pool(4); //创建四个线程的线程池
+    std::vector< std::future<int> > results; 
+    //之前依靠全局变量来从线程中返回异步任务结果
+    //future对象提供访问异步操作结果的机制
 
     for(int i = 0; i < 8; ++i) {
         results.emplace_back(
             pool.enqueue([i] {
-                std::cout << "hello " << i << std::endl;
+                std::cout << "hello " << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " << i << std::endl;
+                std::cout << "world " << std::endl;
                 return i*i;
             })
         );
@@ -24,6 +24,5 @@ int main()
     for(auto && result: results)
         std::cout << result.get() << ' ';
     std::cout << std::endl;
-    
     return 0;
 }
